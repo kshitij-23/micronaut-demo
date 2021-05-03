6623339b5e0743b420b2f045c58638980553b14f
Kshitij <solanki078@gmail.com>
2021-05-03 15:09:27 +0530

    updated..

--- src/main/java/com/ksh/entities/Person.java
+++ src/main/java/com/ksh/entities/Person.java
@@ -8,38 +8,15 @@ import java.io.Serializable;
 
 @Entity
 @Table(name = "person")
+@Data
 public class Person implements Serializable {
 
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    private Long id;
     @Column(name = "name")
     private String name;
     @Column(name = "email")
     private String email;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
--- src/main/java/com/ksh/repositories/PersonRepository.java
+++ src/main/java/com/ksh/repositories/PersonRepository.java
@@ -5,5 +5,5 @@ import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.repository.CrudRepository;
 
 @Repository
-public interface PersonRepository extends CrudRepository<Person, Integer> {
+public interface PersonRepository extends CrudRepository<Person, Long> {
 }
